d147ac8035e75d12b388619f7ac92d4b97e7b914
KunytskaRuslana <kunytska@gmail.com>
2015-06-29 22:22:39 +0300

    FakeProductRepository

---
+++ BS/BS.Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using BS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS.Repositories
+{
+    public interface IProductRepository
+    {
+        List<Product> SelectAll();
+    }
+}
